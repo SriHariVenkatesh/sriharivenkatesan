782096d70cdd2f719329c317b65f7935d291d9fe
SriHariVenkatesh <35689225+SriHariVenkatesh@users.noreply.github.com>
2018-02-02 13:52:54 +0530

    vowel

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	char letter;
+	printf("enter the variable is constant");
+	scanf("%c",&letter);
+            if(letter=='a'||letter=='e'||letter=='i'||letter=='o'|letter=='u')
+            {
+	printf("given variable is vowel");
+
+}
+else
+{
+	printf("given variable is constant");
+}
+ return 0;
+}
